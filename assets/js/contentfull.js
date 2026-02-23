60d0d7de9637c0c74265c48f9d91dc325687eb36
Nikolaj Hansen <nh@itperformance.dk>
2026-02-23 22:33:44 +0100

    more CMS

--- assets/js/contentfull.js
+++ assets/js/contentfull.js
@@ -32,8 +32,33 @@ const client = createClient({
 // Hent entries tagget med et givet tag (eksporteret helper)
 export async function getEntries(tag = "") {
   let res = await client.getEntries({
-    "metadata.tags.sys.id[all]": tag
+    "metadata.tags.sys.id[all]": tag,
   });
 
+  console.log("Contentful response:", res);
+
   return createContentfulIndex(res);
+}
+
+export function createFeature(event) {
+  const article = document.createElement("article");
+
+  const img = document.createElement("img");
+  img.setAttribute("class", "image fit");
+  img.style.objectFit = "cover";
+  img.setAttribute("src", asHttpsUrl(event.fields.file.url));
+  img.setAttribute("alt", event.fields.title);
+
+  const h3 = document.createElement("h3");
+  h3.setAttribute("class", "major");
+  h3.innerHTML = event.fields.title;
+
+  const p = document.createElement("p");
+  p.innerHTML = event.fields.description;
+
+  article.appendChild(img);
+  article.appendChild(h3);
+  article.appendChild(p);
+
+  return article;
 }
